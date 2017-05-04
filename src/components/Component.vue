2f44332dab6aee7b0ad545c42756caa754667d91
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-04 21:04:45 +0100

    Initial commit 🚀

---
+++ src/components/Component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div  v-bind:class="{ active: isActive }" />
+</template>
+
+<script>
+  import dependency from '../lib/dependency';
+
+  export default{
+    name: 'Component',
+
+    data() {
+      return {
+        isActive: dependency({})
+      };
+    },
+
+  };
+</script>
---
+++ src/lib/dependency.js
@@ -0,0 +1,3 @@
+/**
+ * Created by eyerburgh on 04/05/2017.
+ */
