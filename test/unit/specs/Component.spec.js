fc23044c952dc4343461e325e9a2a01406e6f8b9
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-05 12:41:20 +0100

    [Tests] Add failing test case

--- test/unit/specs/Component.spec.js
+++ test/unit/specs/Component.spec.js
@@ -2,9 +2,14 @@ import { mount } from 'avoriaz'
 import Component from '@/components/Component'
 
 describe('Component.vue', () => {
-  it('should render correct contents', () => {
+  it('renders a div with class active when dependency returns true', () => {
     const wrapper = mount(Component)
     expect(wrapper.hasClass('active')).to.equal(true)
+  })
+
+  it('renders a div without class active when dependency returns false', () => {
+    const wrapper = mount(Component)
+    expect(wrapper.hasClass('active')).to.equal(false)
   })
 })
 
