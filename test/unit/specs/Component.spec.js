e76771c3ba8838a5c50f4d8e0b6b9357eb3ef82f
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-07 15:24:28 +0100

    [New] Use vue-loader to stub dependency

--- test/unit/specs/Component.spec.js
+++ test/unit/specs/Component.spec.js
@@ -1,17 +1,19 @@
 import { mount } from 'avoriaz'
-import Component from '@/components/Component'
+import componentFactory from '!!vue-loader?inject!@/components/Component'
 
 describe('Component.vue', () => {
-  it('renders a div with class active when dependency returns true', () => {
-    const wrapper = mount(Component)
-    expect(wrapper.hasClass('active')).to.equal(true)
-  })
+it('renders a div with class active when dependency returns true', () => {
+  const Component = componentFactory({})
+  const wrapper = mount(Component)
+  expect(wrapper.hasClass('active')).to.equal(true)
+})
 
   it('renders a div without class active when dependency returns false', () => {
-    Component.__Rewire__('dependency', () => false)
+    const Component = componentFactory({
+      '../lib/dependency': () => false
+    })
     const wrapper = mount(Component)
     expect(wrapper.hasClass('active')).to.equal(false)
-    Component.__ResetDependency__('dependency')
   })
 })
 
